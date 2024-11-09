e2d57832eb2faec780cbae0a5f32ea14317b772c
F3lipe_htpps000 <felipedellapace16@gmail.com>
2024-11-09 19:44:54 -0300

    Operador: Typeof

---
+++ CFBCursos/Exercicios/Ex.011/script.js
@@ -0,0 +1,9 @@
+var v1 = 10
+var v2 = "10"
+var v3 = v1 === v2
+var v4 = {nome:"Rodrigo"}
+
+console.log(`Valor: ${v1} - Tipo: ${typeof(v1)}`)
+console.log(`Valor: ${v2} - Tipo: ${typeof(v2)}`)
+console.log(`Valor: ${v3} - Tipo: ${typeof(v3)}`)
+console.log(`Valor: ${v4} - Tipo: ${typeof(v4)}`)
