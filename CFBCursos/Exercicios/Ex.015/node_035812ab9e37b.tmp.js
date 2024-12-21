eff58f30a1982cb7147358820cb907440e5bda7f
F3lipe_htpps000 <felipedellapace16@gmail.com>
2024-12-21 18:23:39 -0300

    Re-estudando for in e for of

--- CFBCursos/Exercicios/Ex.015/node_035812ab9e37b.tmp.js
+++
@@ -1,3 +0,0 @@
- for(var ind = 5; ind < 10; ind--) {
-    console.log(`Sukuna, O Rei das Mandições`)
- }
--- CFBCursos/Exercicios/Ex.016/script.js
+++ CFBCursos/Exercicios/Ex.016/script.js
@@ -1,24 +1,13 @@
-const objs = document.getElementsByTagName('div')
-
-for (o of objs) {
-    console.log(o.innerHTML = "Curso")
-}
-
-for (o in objs) {
-    console.log(objs[o].innerHTML)
-}
-
-/*
-
 var num = [10, 20, 30, 40, 50]
 
-for (n in num) {
-     console.log(`${num[n]}`)
+for (i of num) {
+    console.log(i)
+ }
+
+for (i in num) {
+    console.log(num[i])
 }
 
-// [...] É igual a:
-
-for (var ind = 0; ind < num.length; ind ++) {
-    console.log(`${num[ind]}`)
-} 
-*/
+for(var i = 0; i < num.length; i++) {
+    console.log(num[i])
+}
