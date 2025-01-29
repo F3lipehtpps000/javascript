4055247885eaf81eae5c432ba25888462a3f97dd
F3lipe_htpps000 <felipedellapace16@gmail.com>
2025-01-29 19:02:56 -0300

    Terminei (não é amanhã)

--- CFBCursos/Exercicios/Ex.030/script(2).js
+++ CFBCursos/Exercicios/Ex.030/script(2).js
@@ -1,1 +1,5 @@
-const divTodas = [...document.getElementsByTagName("div")]
+const divTodas = [...document.getElementsByTagName('div')]
+const query_divTodas = document.querySelectorAll('div > p')
+
+console.log(query_divTodas)
+// console.log(divTodas)
