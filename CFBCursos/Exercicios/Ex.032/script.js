747daac8124979a0908ebaba1c63eb8083ec03ba
F3lipe_htpps000 <felipedellapace16@gmail.com>
2025-02-11 18:00:04 -0300

    Fazendo o código desse exercicio

--- CFBCursos/Exercicios/Ex.032/script.js
+++ CFBCursos/Exercicios/Ex.032/script.js
@@ -1,6 +1,25 @@
-var cursos = [...document.getElementsByClassName('curso')]
+var cursos = document.getElementsByClassName('curso')
+var btnCopiar = document.getElementById('btn_cop')
+var caixa1 = document.getElementById('caixa1')
+var caixa2 = document.getElementById('caixa2')
+
 function selecionar() {
-    cursos.map((el, i) => {
-        
-    })
+    this.classList.toggle('destaque')
 }
+
+for (var i = 0; i < cursos.length; i++) {
+    cursos[i].addEventListener('click', selecionar)
+}
+
+btnCopiar.addEventListener('click', function() {
+    var operacao = this.classList.contains('destaque')
+    if (operacao = true) {
+        console.log('Aaarrrrhhhggg')
+    } else {
+        var selecionados = document.querySelectorAll('.destaque')
+        selecionados.forEach(function(el) {
+            caixa2.appendChild(el)
+            el.classList.remove('destaque')
+        })
+    }
+})
