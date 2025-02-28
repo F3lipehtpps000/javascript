223816f161466c3ed0cceeee1fdbb19fcf8f3a80
F3lipe_htpps000 <felipedellapace16@gmail.com>
2025-02-28 17:57:02 -0300

    Adicionei o JS ao projeto

--- Projetinhos Próprios/projeto-pomodoro/script.js
+++ Projetinhos Próprios/projeto-pomodoro/script.js
@@ -1,6 +1,34 @@
-var data = new Date()
-var horas = data.getHours()
-var minutos = data.getMinutes()
+const timer = document.getElementById('timer')
+const iniciar = document.getElementById('iniciar')
+const resetar = document.getElementById('resetar')
 
-var timer = document.getElementById('timer')
-timer.innerText = `${horas}:${minutos}`
+var timeLeft= 25 * 60
+var intervalo
+
+function updadeTimer() {
+    const minutos = Math.floor(timeLeft / 60)
+    const segundos = timeLeft % 60
+
+    timer.textContent = `${String(minutos).padStart(2, '0')}:${String(segundos).padStart(2, '0')}`
+
+    if (timeLeft === 0) {
+        clearInterval(intervalo)
+        alert("O tempo acabou. Tenha seu descanço!")
+    }
+    else {
+        timeLeft--
+    }
+}
+
+iniciar.addEventListener('click', () => {
+    if (!intervalo) {
+        intervalo = setInterval(updadeTimer, 1000)
+    }
+})
+
+resetar.addEventListener('click', () => {
+    clearInterval(intervalo)
+    intervalo = null
+    timeLeft = 25 * 60
+    timer.textContent = "25:00"
+})
